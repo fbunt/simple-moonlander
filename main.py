c061f097500045b91a990863e979034d29242473
Fred Bunt <hangmanhankr@gmail.com>
2015-01-15 14:38:48 -0700

    Improve documentation in main.py.

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ Interesting Paths
 
 # Initial angular position of the lander in orbit about Earth
 alpha = np.radians(30.)
-# Initial velocity boost to send the lander toward the Moon.
+# Initial velocity boost to kick the lander out of Earth orbit..
 # Units are in terms of the Earth-Moon distance.
 vkick = 3.003
 MAX_ITERATIONS = int(1E+5)
-
-s = Simulation(alpha, vkick, MAX_ITERATIONS, "lander_sim")
+# The name used for this simulation.
+sim_name = "lander_sim"
+s = Simulation(alpha, vkick, MAX_ITERATIONS, sim_name)
 s.run()
-s.write_data("lander_sim_data.txt")
+s.write_data(sim_name + "_data.txt")
 s.plot_system()
 s.plot_lander_moon_distance()
 
